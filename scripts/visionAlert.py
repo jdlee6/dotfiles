978dc50fe5e5e3aefe1d7df979ea21625680c248
joe <jdlee6@buffalo.edu>
2019-12-14 21:54:41 -0500

    vision alert script & updated configs

---
+++ scripts/visionAlert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#script to alert myself every 25 minutes to rest my eyes
+
+import time
+import subprocess
+
+count = 1500
+while count > 0:
+    print(f'Counting down . . . Remaining seconds left: {count} seconds . . .')
+    time.sleep(1)
+    count -= 1
+    if count == 0:
+        subprocess.Popen(
+            ['xdg-open', '/home/joe/Downloads/niceudes.mp3'])
+        print('Look out your window right now!\n')
+        # sleep the program for 25 seconds to rest eyes
+        # alert again when time is up and restart counter
+        time.sleep(40)
+        subprocess.Popen(['xdg-open', '/home/joe/Downloads/niceudes.mp3'])
+        time.sleep(3)
+        print('Restarting the program in 3 seconds . . .')
+        count = 1500
